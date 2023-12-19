f24cb25f45f059df14f8fe9fae8ade44e7ccd32d
Alin Grigorescu <alin.grig87@gmail.com>
2023-12-19 20:35:53 +0200

    update: polymorphism and overloading

--- OOP/src/main/java/org/example/Main.java
+++ OOP/src/main/java/org/example/Main.java
@@ -4,33 +4,60 @@ import org.example.abstraction.Cerc;
 import org.example.composition.Adresa;
 import org.example.composition.Persoana;
 import org.example.encapsulation.ContBancar;
-import org.example.inheritance.Animal;
+//import org.example.inheritance.Animal;
 import org.example.inheritance.Caine;
+import org.example.overloading.Calculator;
+import org.example.polymorphism.Dog;
+import org.example.polymorphism.Animal;
+import org.example.polymorphism.Cat;
+
+
+
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    static void scoateSunet(Animal a) {
+        a.scoateSunet();
+    }
     public static void main(String[] args) {
-        Persoana p = new Persoana("Costel");
-        System.out.println(p.getNume());
-        p.setNume("Viorel");
-        System.out.println(p.getNume());
+//        Persoana p = new Persoana("Costel");
+//        System.out.println(p.getNume());
+//        p.setNume("Viorel");
+//        System.out.println(p.getNume());
+//
+//        Cerc c = new Cerc(200,100, 20);
+//        c.deseneaza();
+//
+//        ContBancar contulLuiAndrei = new ContBancar(2000);
+//        contulLuiAndrei.depune(500);
+//        contulLuiAndrei.retrage(2800);
+//
+//        Persoana mirel = new Persoana("Mirel", new Adresa("Calea Bucuresti", "Targu Jiu", "102323"));
+//        mirel.prezentare();
+//
+//        Animal a = new Caine("Azorel");
+//        a.scoateSunete();
+//
+//        Caine caine = new Caine("Bobita");
+//        caine.latra();
+//        caine.scoateSunete();
 
-        Cerc c = new Cerc(200,100, 20);
-        c.deseneaza();
+        Animal a1 = new Dog();
+        Animal a2 = new Cat();
 
-        ContBancar contulLuiAndrei = new ContBancar(2000);
-        contulLuiAndrei.depune(500);
-        contulLuiAndrei.retrage(2800);
+        // static polymorphism
+        a1.scoateSunet();
+        a2.scoateSunet();
 
-        Persoana mirel = new Persoana("Mirel", new Adresa("Calea Bucuresti", "Targu Jiu", "102323"));
-        mirel.prezentare();
+        // dynamic dispatch
+        scoateSunet(a1);
+        scoateSunet(a2);
 
-        Animal a = new Caine("Azorel");
-        a.scoateSunete();
+        Calculator calculator = new Calculator();
+        calculator.aduna(1, 2);
+        calculator.aduna(2,3,4);
+        calculator.aduna(2.0, 1);
 
-        Caine caine = new Caine("Bobita");
-        caine.latra();
-        caine.scoateSunete();
     }
 }
